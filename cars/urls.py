8eeeeb9bb765462259265a90623ebc2b5034519c
neerojstha <100852394+neerojstha@users.noreply.github.com>
2026-05-19 10:49:37 +0000

    car deatils added

--- cars/urls.py
+++ cars/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('car/<int:id>/', views.car_detail, name='car_detail'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Car
 
 
 def home(request):
     cars = Car.objects.all().order_by('-created_at')
     return render(request, 'index.html', {'cars': cars})
+
+
+def car_detail(request, id):
+    car = get_object_or_404(Car, id=id)
+
+    return render(request, 'car_detail.html', {
+        'car': car
+    })
