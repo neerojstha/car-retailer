be336d987d6b6efe048d6569adcbdea19f873b8d
neerojstha <100852394+neerojstha@users.noreply.github.com>
2026-05-20 10:26:35 +0000

    multiple pictures

--- cars/models.py
+++ cars/models.py
@@ -7,9 +7,9 @@ class Car(models.Model):
     year = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-
-    # Main thumbnail image
     image = models.ImageField(upload_to='cars/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
