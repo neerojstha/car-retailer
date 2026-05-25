88d38fa9b055b1ae5976c383cf38564aa90e46bf
neerojstha <100852394+neerojstha@users.noreply.github.com>
2026-05-25 12:19:35 +0000

    Added Sold Field

--- cars/models.py
+++ cars/models.py
@@ -15,6 +15,8 @@ class Car(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
 
+    sold = models.BooleanField(default=False)
+
     def __str__(self):
         return self.title
 
